35eda79e1a190d61e66a3a774aa3b90101dec2f8
EversonDoNascimento <everson25nascimento@gmail.com>
2025-06-13 07:37:31 -0300

    feat: Change default color status bar for white color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
-import "./../global.css";
+import { StatusBar } from "react-native";
 import { StepAuthContextProvider } from "../contexts/stepAuthContext";
+import "./../global.css";
 const RootLayout = () => {
   return (
     <StepAuthContextProvider>
+      <StatusBar barStyle="light-content"></StatusBar>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
