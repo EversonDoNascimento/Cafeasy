131c91c1cfd9a1547080a42e0705de9446541f23
EversonDoNascimento <everson25nascimento@gmail.com>
2025-06-28 08:14:26 -0300

    feat: Created data mock

---
+++ CONSTANTS/coffee.ts
@@ -0,0 +1,44 @@
+export const coffee = [
+  {
+    id: 1,
+    name: "Coromel Machioto",
+    price: 2.99,
+    image:
+      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 2,
+    name: "Coromel Machioto",
+    price: 2.99,
+    image:
+      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 3,
+    name: "Coromel Machioto",
+    price: 2.99,
+    image:
+      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 4,
+    name: "Coromel Machioto",
+    price: 2.99,
+    image:
+      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 5,
+    name: "Coromel Machioto",
+    price: 2.99,
+    image:
+      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 6,
+    name: "Coromel Machioto",
+    price: 2.99,
+    image:
+      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+];
